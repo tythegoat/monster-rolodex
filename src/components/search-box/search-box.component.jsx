b0ac9e9256b1715e147f5ecea57d8a63638da854
tythegoat <61744367+tythegoat@users.noreply.github.com>
2020-11-02 18:16:30 +0100

    Adding files for github pages

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import './search-box.styles.css';
+
+export const SearchBox = props => (
+    <input className='search' type='search' placeholder={props.placeHolder} onChange={props.changeHandler}/>
+);
